430d96b1e99ea442e7e90e17395657df2fe36e12
Julia <julia.tiemi@bagy.com.br>
2022-10-16 11:46:03 -0300

    Fix get contract so it returns only if the contract belongs to the profile calling

--- src/app.js
+++ src/app.js
@@ -2,19 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
+const { Op } = require('sequelize');
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
-/**
- * FIX ME!
- * @returns contract by id
- */
 app.get('/contracts/:id', getProfile, async (req, res) => {
   const { Contract } = req.app.get('models');
   const { id } = req.params;
-  const contract = await Contract.findOne({ where: { id } });
+  const { id: profileId } = req.profile;
+  const contract = await Contract.findOne({
+    where: {
+      [Op.and]: [
+        { id },
+        {
+          [Op.or]: [{ contractorId: profileId }, { clientId: profileId }],
+        },
+      ],
+    },
+  });
   if (!contract) return res.status(404).end();
   res.json(contract);
 });
